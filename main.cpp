0dafacf3e9ab060071ad0c7e5fc86d161410d598
iceman2146 <30576571+iceman2146@users.noreply.github.com>
2023-04-28 21:42:05 +0300

    first commit

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include "version.h"
 
 int main()
 {
-    int version=0;
-    std::cout<<"VERSION: "<<version<<std::endl;
+    //int version=0;
+    std::cout<<"VERSION: "<<PROJECT_VERSION_PATCH<<std::endl;
     std::cout<<"HELLO WORLD!"<<std::endl;
     
 
