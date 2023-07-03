0bed36837a297117c18c59e7f5ab3962b71379eb
iceman2146 <iceman2146@mail.ru>
2023-07-03 21:59:15 +0300

    ()*&

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,8 @@ int main()
     //int version=0;
     std::cout<<"VERSION: "<<version()<<std::endl;
     std::cout<<"HELLO WORLD!"<<std::endl;
+    std::cout<<"HELLO WORLD!"<<std::endl;
+    std::cout<<"HELLO WORLD!"<<std::endl;
     
 
     return 666;
